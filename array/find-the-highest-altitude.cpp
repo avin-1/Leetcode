fccb62b7a269aaa62adbba177fcf124268894a9f
Avinash Bhurke <149309916+avin-1@users.noreply.github.com>
2026-05-28 18:51:00 +0530

    LC-find-the-highest-altitude [Runtime: 0 ms; Memory: 10.8 MB]

--- array/find-the-highest-altitude.cpp
+++ array/find-the-highest-altitude.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int n  = gain.size();
-        int sum =  0;
-        int maxsum = 0;
-        for(int i = 0;i<n;i++){
-            sum = sum+gain[i];
-            maxsum = max(maxsum,sum);
+        vector<int> altitude;
+        altitude.push_back(0);
+        // next element in vector will be i and gain[i];
+        // how will i implement this
+        int a = 0;
+        for(int i = 0;i<gain.size();i++){
+            altitude.push_back( altitude[a]+gain[i]);
+            a++;
         }
-        return maxsum;
+        return *max_element(altitude.begin(),altitude.end());
     }
 };
